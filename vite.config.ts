867410b0ead0c2775560de3a1deead15c45ac319
ajeetmishra1943 <75467611+ajeetmishra1943@users.noreply.github.com>
2024-09-21 19:29:39 +0530

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -10,29 +10,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  const sanitizeKey = (key) => key.replace(/\W/g, '_')
-
-  const envWithProcessPrefix = Object.entries(env).reduce(
-    (prev, [key, val]) => {
-      // Sanitize the key and add it to the object
-      return {
-        ...prev,
-        [`process.env.${sanitizeKey(key)}`]: JSON.stringify(val)
-      }
-    },
-    {}
-  )
-
   return defineConfig({
     plugins: [react(), tsconfigPaths()],
     base: env.VITE_ROUTER_BASE_URL || '/',
-    define: envWithProcessPrefix,
-    // Add this to prevent Vite from trying to handle process.env
-    resolve: {
-      alias: {
-        'process.env': JSON.stringify({}),
-      },
-    },
+    define: ({
+      'process.env' : env
+    }),
 }
   )
 }
